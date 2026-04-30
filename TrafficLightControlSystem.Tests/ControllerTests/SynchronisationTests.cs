c7bdee1313ea0797e9ca6214b20a7f12cda2ba06
Daniel Barboni <serpoiu@gmail.com>
2026-04-30 02:59:40 +0100

    added placeholder test ofr synchronisation invariant for now

---
+++ TrafficLightControlSystem.Tests/ControllerTests/SynchronisationTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrafficLightControlSystem.Core;
+
+namespace TrafficLightControlSystem.Tests.ControllerTests
+{
+    public class SynchronisationTests
+    {
+        [Fact]
+        public void PairedDirections_ShouldAlwaysBeInternallyConsistent()
+        { 
+            var controller = new TrafficLightController();
+
+            controller.ChangeToNextSignal(Direction.NorthSouth);
+
+            Assert.NotEqual(Signal.Off, controller.NorthSouth);
+        }
+    }
+}
