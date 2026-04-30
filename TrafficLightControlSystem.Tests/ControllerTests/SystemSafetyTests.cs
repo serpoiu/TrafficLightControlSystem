d41fb54af01ee45689a88bfc79af330acb87c8e6
Daniel Barboni <serpoiu@gmail.com>
2026-04-30 12:01:24 +0100

    added system level safety tests

---
+++ TrafficLightControlSystem.Tests/ControllerTests/SystemSafetyTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrafficLightControlSystem.Core;
+
+namespace TrafficLightControlSystem.Tests.ControllerTests
+{
+    public class SystemSafetyTests
+    {
+        [Fact]
+        public void System_ShouldNeveAllowBothDirectionsGreen()
+        { 
+            var controller = new TrafficLightController();
+
+            controller.ChangeToNextSignal(Direction.NorthSouth);
+            controller.ChangeToNextSignal(Direction.NorthSouth);
+
+            controller.ChangeToNextSignal(Direction.EastWest);
+            controller.ChangeToNextSignal(Direction.EastWest);
+
+            Assert.Equal(Signal.Green, controller.NorthSouth);
+            Assert.True(controller.EastWest == Signal.Red);
+        }
+
+        [Fact]
+        public void System_ShouldNotAllowTrafficDuringPedestrianCrossing()
+        {
+            var controller = new TrafficLightController();
+
+            controller.RequestPedestrianCrossing();
+            controller.Tick(TimeSpan.Zero);
+
+            Assert.Equal(Signal.Red, controller.NorthSouth);
+            Assert.Equal(Signal.Red, controller.EastWest);
+        }
+
+        [Fact]
+        public void System_ShouldNotAllowTransitionFromOffState()
+        {
+            var controller = new TrafficLightController();
+
+            controller.TriggerSignalProgressionFailure();
+
+            controller.ChangeToNextSignal(Direction.NorthSouth);
+
+            Assert.Equal(Signal.Off, controller.NorthSouth);
+            Assert.Equal(Signal.Off, controller.EastWest);
+        }
+    }
+}
